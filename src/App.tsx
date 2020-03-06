f97fd66117e8b594e0736e7cdc6797ae89f3e0f2
markellekelly <mkelly23@calpoly.edu>
2020-03-05 19:20:14 -0800

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+
 }
 
-export default App;
+interface AppState {
+
+}
+
+export class App extends React.Component<
+    AppProps, AppState
+> {
+    constructor (props:any) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div id="page">
+               
+            </div>
+        )
+    }
+}
