63a1c9cbf619949227f9bd4b85f9bc0581ac24a1
jennalandy <jennalandy0@gmail.com>
2020-03-05 19:54:33 -0800

    header and nav

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,24 @@ export class App extends React.Component<
     render() {
         return(
             <div id="page">
-               
+                <div id="header">
+                    <h1>
+                        Mimosas and Business
+                    </h1>
+                </div>
+                <div id="nav">
+                    <ul>
+                        <li>
+                            Home
+                        </li>
+                        <li>
+                            Episodes
+                        </li>
+                        <li>
+                            About
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from "react-dom";
 
 import { App } from "./App";
 
+import './style/main.css';
+
 ReactDOM.render(
   <App />,
   document.getElementById('app')
